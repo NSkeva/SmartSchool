28733cd83cf0555c564587c2df56f3ad9911446f
NSkeva <nikola.skeva1@gmail.com>
2021-09-03 11:43:20 +0200

    Firebase 2

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigation/stackNavigator';
+
+import './Firebase';
+
 export default function App() {
   return (
     <NavigationContainer>
